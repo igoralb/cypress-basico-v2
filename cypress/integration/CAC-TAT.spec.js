0b658485cf128a62a44ba9a7b4bf84f6aba1159f
Igor Bastos Albuquerque <igor.bastos@live.com>
2025-12-10 11:40:10 -0300

    adiciona teste aos inputs checkbos e radio

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -19,7 +19,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     })
 
     it('preenche os campos obrigatórios e envia o formulário', function () {
-      
+
         cy.get('#firstName')
             .click()
             .type("Igor")
@@ -27,20 +27,20 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#lastName')
             .click()
             .type("Bastos")
-            
+
         cy.get('#email')
             .click()
             .type("igor.bastos@teste.com")
 
-        cy.get('#open-text-area').click().type("Este é apenas um teste",{delay:0})    
+        cy.get('#open-text-area').click().type("Este é apenas um teste", { delay: 0 })
 
         cy.get('.button').click()
 
         cy.get('.success').should('be.visible')
 
     })
-    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
-        
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
+
         cy.get('#firstName')
             .click()
             .type("Igor")
@@ -48,12 +48,12 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#lastName')
             .click()
             .type("Bastos")
-            
+
         cy.get('#email')
             .click()
             .type("emailinvalido.com")
 
-        cy.get('#open-text-area').click().type("Este é apenas um teste",{delay:0})    
+        cy.get('#open-text-area').click().type("Este é apenas um teste", { delay: 0 })
 
 
         cy.get('button[type="submit"]').click()
@@ -61,35 +61,35 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('.error').should('be.visible')
     })
 
-    it('preenche campo telefone com valor não numerico', function() {
+    it('preenche campo telefone com valor não numerico', function () {
 
         cy.get('#phone').click().type('teste')
-        cy.get('#phone').should('have.value','')
+        cy.get('#phone').should('have.value', '')
     })
 
-    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
-         cy.get('#firstName')
+    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function () {
+        cy.get('#firstName')
             .click()
             .type("Igor")
 
         cy.get('#lastName')
             .click()
             .type("Bastos")
-            
+
         cy.get('#email')
             .click()
             .type("igor.bastos@teste.com")
 
-        cy.get('#open-text-area').click().type("Este é apenas um teste",{delay:0})    
+        cy.get('#open-text-area').click().type("Este é apenas um teste", { delay: 0 })
 
-        cy.get('#phone-checkbox').click()
+        cy.get('#phone-checkbox').check()
 
         cy.get('.button').click()
 
-        cy.get('.error').should('be.visible')  
+        cy.get('.error').should('be.visible')
     })
-    it('preenche e limpa os campos nome, sobrenome, email e telefone',function(){
-         cy.get('#firstName')
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function () {
+        cy.get('#firstName')
             .click()
             .type("Igor")
         cy.get('#firstName').clear().should('have.value', '')
@@ -98,7 +98,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .click()
             .type("Bastos")
         cy.get('#lastName').clear()
-            
+
         cy.get('#email')
             .click()
             .type("igor.bastos@teste.com")
@@ -111,32 +111,123 @@ describe('Central de Atendimento ao Cliente TAT', function () {
 
         cy.get('#open-text-area')
             .click()
-            .type("Este é apenas um teste",{delay:0})  
+            .type("Este é apenas um teste", { delay: 0 })
         cy.get('#open-text-area').clear().should('have.value', '')
 
     })
-    
-    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios',function(){
-        
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function () {
+
         cy.get('button[type="submit"]').click()
 
-        cy.get('.error').should('be.visible')  
+        cy.get('.error').should('be.visible')
     })
 
     //comandos costumizados para evitar duplicidade de código -> comando costumizados(preferencial e evita duplicacao de codigo) vs page objects
-    it.only('Evia formualrio com sucesso usando comandosocutmizados', function(){
+    it('Evia formualrio com sucesso usando comandosocutmizados', function () {
         cy.fillMandatoryFieldsAndSubmit()
 
         cy.get('.success').should('be.visible')
 
     })
 
-    //uso do .contains() -> seletor,texto especifico
-    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios',function(){
-        
+    //uso do .contains() -> seletor,texto especifico 
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function () {
+
         cy.contains('button', 'Enviar').click()
 
-        cy.get('.error').should('be.visible')  
+        cy.get('.error').should('be.visible')
     })
+
+    //uso de select options de forma aleatoria 
+    it('Selecionar uma opção de forma aleatoria', () => {
+
+        cy.get('select#product option')
+            .not('[disabled]')
+            .as('options')
+            .its('length', { log: false }).then(n => {
+                cy.get('@options', { log: false }).then($options => {
+                    const randomOptionIndex = Cypress._.random(n - 1)
+                    const randomOptionText = $options[randomOptionIndex].innerText
+                    cy.get('select').select(randomOptionText)
+                })
+            })
+    })
+    // da certo ser no get a chamada pelo elemento select ou pelo id  dos products
+    it('seleciona produto (seleciona um produto (YouTube) por seu texto', () => {
+        cy.get('select')
+            .select('YouTube')
+            .should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+        cy.get('#product')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+    })
+
+    it('seleciona um produto (Blog) por seu índice', () => {
+        cy.get('select')
+            .select(1)
+            .should('have.value', 'blog')
+    })
+
+    // inputs tipo radio ou checkbox -por questões semanticas da certo o .check mas tbm funciona .click
+    it('Selecionna button radio', () => {
+        cy.get('input[type="radio"]')
+            .check()
+
+    })
+
+    // o check() aceita um valor
+    it('marca o tipo de atendimento "Feedback"', () => {
+        cy.get('input[type="radio"] ')
+            .check('feedback')
+            .should('have.value', 'feedback')
+
+    })
+
+    it('marca o tipo de atendimento "Feedback" e checar', () => {
+        cy.get('input[type="radio"][value="feedback"] ')
+            .check()
+            .should('have.value', 'feedback')
+            .should('be.checked')
+    })
+
+    //selecionar varios checks e testar um a um
+    it('marca cada tipo de atendimento', () => {
+        cy.get('input[type="radio"]')
+            .should('have.length', 3)
+            .each(function ($radio) {
+                cy.wrap($radio).check()
+                cy.wrap($radio).should('be.checked')
+            })
+    })
+    //ESSA FORMA NÃO É PERFORMÁTICA
+
+    // agora vamos ver os checkbox 
+    // it.only('marca ambos checkboxes, depois desmarca o último', ()=>{
+    //     cy.get('input[type="checkbox"]')
+    //         .each(function($checkbox){
+    //             //lemprar de utilizar o wrap como forma de lidar com o objeto - o get espera uma string
+    //             cy.wrap($checkbox).check().should('be.checked')
+
+    //         })
+    // })
+
+    //FORMA CORRETA
+    it.only('marca ambos checkboxes, depois desmarca o último', () => {
+        cy.get('#check input[type="checkbox"]')
+            .check()
+            .should('be.checked')
+
+        cy.get('input[type="checkbox"]')
+            .last()                     
+            .uncheck()              
+            .should('not.be.checked')
+    })
+    
+    //Seção 7: Fazendo upload de arquivos com Cypress
+
 
 })  
